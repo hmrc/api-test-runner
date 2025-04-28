fcb47f6a48786fa393218675ec5d3eef37f28cbf
Victor Osipov <osipovvictor1994@gmail.com>
2025-04-28 20:20:38 +0100

    align logging library with platform

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,10 +4,10 @@ object Dependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "com.typesafe"         % "config"                  % "1.4.3",
-    "org.playframework"   %% "play-ahc-ws-standalone"  % "3.0.6",
-    "org.playframework"   %% "play-ws-standalone-json" % "3.0.6",
+    "org.playframework"   %% "play-ahc-ws-standalone"  % "3.0.7",
+    "org.playframework"   %% "play-ws-standalone-json" % "3.0.7",
     "com.vladsch.flexmark" % "flexmark-all"            % "0.64.8",
     "org.scalatest"       %% "scalatest"               % "3.2.19",
-    "org.slf4j"            % "slf4j-simple"            % "2.0.13"
+    "ch.qos.logback"       % "logback-classic"         % "1.5.18"
   )
 }
